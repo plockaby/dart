7f988de1478749a4717df163efce585be0195ee2
Paul Lockaby <paul@paullockaby.com>
2017-11-13 05:56:33 +0000

    removing unused code from setup.py

--- agent/setup.py
+++ agent/setup.py
@@ -1,18 +1,11 @@
-import os
 import sys
 from setuptools import setup, find_packages
 
 # tell python to not write bytecode files
 sys.dont_write_bytecode = True
 
-# get the version out of the package itself
-path = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, "{}/lib".format(path))
-from dart.agent import __version__  # noqa
-
 setup(
     name='dart-agent',
-    version=__version__,
     scripts=['bin/dart-agent'],
     package_dir={'': 'lib'},
     packages=find_packages(where='lib', exclude=('tests', 'tests.*')),
