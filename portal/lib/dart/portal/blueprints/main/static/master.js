9db8b0256d44228cb230bb4ec553e0218d8b909f
Paul Lockaby <paul@paullockaby.com>
2019-10-23 03:59:56 +0000

    adding bootstrap, jquery, moment.js, bootstrap-typeahead and bootstrap-table

---
+++ portal/lib/dart/portal/blueprints/main/static/master.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    // namespace for the program
+    var dart = {};
+
+    // expose all the functions here under the window.dart
+    window.dart = dart;
+    return window.dart;
+})();
